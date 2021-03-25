288f9e0ac1d08ba4f84ecee6d58ba19eb0919282
emily chachian <emilychachian@hotmail.com>
2021-03-25 18:33:00 -0300

    feat: Fetching API data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useContext } from "react";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
+import UserList from "./components/UserList/UserList"
+import styles from "./styles/styles.scss";
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(async () => {
+    await fetch('https://api.github.com/orgs/grupotesseract/public_members')
+    .then((res) => res.json())
+    .then((data) => {
+    setUsers(data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <SearchBar />
+      <UserList users={users}/> 
+    </React.Fragment>
   );
+
 }
 
 export default App;
